bcd750e27bdedd83eb0b31b46c4d46366bbfad5b
Brian Simmons <bsimmons01@gmail.com>
2025-06-03 10:08:41 -0700

    Initial Commit

---
+++ ToastMessaging/ToastMessagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastMessagingApp.swift
+//  ToastMessaging
+//
+//  Created by Brian Simmons on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ToastMessagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
